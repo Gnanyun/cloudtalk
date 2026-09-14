fb7401e4fe453f364eeeb1eb146daf913acd4ce7
Gnanyun <a28293185697@163.com>
2026-09-14 20:50:05 +0800

    Update app.py

--- cloudtalk-flask/app.py
+++ cloudtalk-flask/app.py
@@ -1,4 +1,8 @@
-
+import os
+import uuid
+import sqlite3
+import hashlib
+import secrets
 from flask import Flask, request, jsonify, send_from_directory, Response, session
 from werkzeug.security import generate_password_hash, check_password_hash
 from PIL import Image  # 需要 pip install Pillow
